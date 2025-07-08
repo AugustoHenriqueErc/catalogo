efa6cd6aeb68237da2c868c40342478925f2e47c
Augusto Henrique <aaugustohenrique05@gmail.com>
2025-07-08 08:31:58 -0300

    adicionando páginas, realizando mudanças no css e js

--- js/script.js
+++ js/script.js
@@ -1,5 +1,5 @@
 document.addEventListener('DOMContentLoaded', function () {
-    const page = window.location.pathname.split("/").pop(); 
+    const page = window.location.pathname.split("/").pop();
     const background = document.querySelector(".about_album");
 
     switch (page) {
@@ -8,7 +8,16 @@ document.addEventListener('DOMContentLoaded', function () {
             break;
         case "moral_panic.html":
             background.style.backgroundImage = "url('img/nbt.cover.jpg')";
-            break;    
+            break;
+        case "luv_is_rage_2.html":
+            background.style.backgroundImage = "url('img/luv_cover.jpg')";
+            break;
+        case "die_lit.html":
+            background.style.backgroundImage = "url('img/carti_cover.webp')";
+            break;
+        case "love_letter_to_you_3.html":
+            background.style.backgroundImage = "url('img/tr_cover.jpg')";
+            break;
         default:
             background.style.backgroundImage = "url('img/bg_default.jpg')";
     }
